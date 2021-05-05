216585ad17252dfa2138842e373560fc3826e980
FireLeon <82052636+FireLeon@users.noreply.github.com>
2021-05-05 22:25:17 +0200

    Add files via upload

---
+++ discord1.py
@@ -0,0 +1,22 @@
+open adresse 'https://www.discord/developerportal.com'
+get data r acess to p:
+	is true:
+		----finished to
+		----1
+		----10
+		----20
+		----30
+		-----40
+		----50
+		---65
+1 3 4 7 1 5 9 4
+1 4 5 1 4 7 4 8
+3 5 6 2 5 8 1 0
+       ---70
+downloadint content........{80 %}
+[INFO]dont use VPN....
+turned off!!!
+
+ content is hacked 100 %
+ 
+ now you can become $ money
